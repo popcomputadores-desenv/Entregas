3423297b6009822da9f07a0ecf7ec870ba5637a9
Robson Gonçalves <robson@masterhub.com.br>
2019-11-19 14:13:35 -0300

    ...

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/* Driver App Configuration */
+
+var krms_driver_config ={		
+	'ApiUrl':"https://app.masterhub.com.br/driver/api",	
+	'DialogDefaultTitle':"WJK Entregas",
+	'mapboxToken' : 'pk.eyJ1Ijoic293Y3JlYXRpdmUiLCJhIjoiY2p3czlsMXpmMDA1eDQ5bHl5Y291eGl6bCJ9.JJpkg9ZEWSzFPcqI5LmQrg',
+	'APIHasKey':"f0b8922f6edcb9d46f7f33e867e70e7d",
+	'debug': false 
+};
