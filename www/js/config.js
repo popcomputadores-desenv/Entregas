4a71ad780095098def5126b6aafb56e2d3922961
Robson <robson@masterhub.com.br>
2020-04-07 00:59:40 -0300

    initial

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/* Driver App Configuration */
+
+var krms_driver_config ={		
+	'ApiUrl':"https://piloto.masterhub.com.br/driver/api",	
+	'DialogDefaultTitle':"Master Hub®",
+	'mapboxToken' : 'pk.eyJ1Ijoic293Y3JlYXRpdmUiLCJhIjoiY2p3czlsMXpmMDA1eDQ5bHl5Y291eGl6bCJ9.JJpkg9ZEWSzFPcqI5LmQrg',
+	'APIHasKey':"4m1DDci5rKsyeNlmkwZlSsQDCpg=",
+	'debug': true 
+};
