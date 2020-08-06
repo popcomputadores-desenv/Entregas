5e257f7632c243da9614ff6bc808fed522b38ef9
Robson <robson@masterhub.com.br>
2020-08-06 12:25:08 -0300

    Envio inicial

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/* Driver App Configuration */
+
+var krms_driver_config ={		
+	'ApiUrl':"https://app.masterhub.com.br/driver/api",	
+	'DialogDefaultTitle':"Entregadores",
+	'mapboxToken' : 'pk.eyJ1Ijoic293Y3JlYXRpdmUiLCJhIjoiY2p3czlsMXpmMDA1eDQ5bHl5Y291eGl6bCJ9.JJpkg9ZEWSzFPcqI5LmQrg',
+	'APIHasKey':"4m1DDci5rKsyeNlmkwZlSsQDCpg=",
+	'debug': true 
+};
