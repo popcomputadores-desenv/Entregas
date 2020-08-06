a6e60f730c515fc2a0c1a9c62697c862a854cd33
Robson <robson@masterhub.com.br>
2020-08-06 12:25:47 -0300

    Envio inicial

--- www/js/config.js
+++ www/js/config.js
@@ -5,5 +5,5 @@ var krms_driver_config ={
 	'DialogDefaultTitle':"Entregadores",
 	'mapboxToken' : 'pk.eyJ1Ijoic293Y3JlYXRpdmUiLCJhIjoiY2p3czlsMXpmMDA1eDQ5bHl5Y291eGl6bCJ9.JJpkg9ZEWSzFPcqI5LmQrg',
 	'APIHasKey':"4m1DDci5rKsyeNlmkwZlSsQDCpg=",
-	'debug': true 
+	'debug': false 
 };
